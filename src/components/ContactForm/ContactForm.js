8071eb1426a4b9620fb77199e445293da20c1efc
galka-travinskaya <galka03031988@rambler.ru>
2021-03-05 11:13:45 +0200

    hw

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,43 +1,44 @@
-import { Component } from "react";
-import { connect } from "react-redux";
-import { CSSTransition } from "react-transition-group";
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
 
-import contactsOperations from "../../redux/contacts/contacts-operations";
-import { getContacts } from "../../redux/contacts/contacts-selectors";
-import Alert from "../Alert";
+import contactsOperations from '../../redux/contacts/contacts-operations';
+import { getContacts } from '../../redux/contacts/contacts-selectors';
+import Alert from '../Alert';
 
-import s from "./ContactForm.module.css";
-import alert from "../../transition/Transition.module.css";
+import s from './ContactForm.module.css';
+import alert from '../../transition/Transition.module.css';
 
 class ContactForm extends Component {
   state = {
-    name: "",
-    number: "",
+    name: '',
+    number: '',
     showAlert: false,
-    errorMessage: "",
+    errorMessage: '',
   };
 
-  handleChange = (e) => {
+  handleChange = e => {
     const { name, value } = e.currentTarget;
     this.setState({
       [name]: value,
     });
   };
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     const { name, number } = this.state;
     if (!name || !number) {
-      this.toggleAlert("The list is empty");
+      this.toggleAlert('The list is empty');
       return;
     }
 
     if (
       this.props.contacts.find(
-        ({ name }) => name.toLowerCase() === this.state.name.toLowerCase()
+        ({ name }) => name.toLowerCase() === this.state.name.toLowerCase(),
       )
     ) {
-      this.toggleAlert("Contact is already exist");
+      this.toggleAlert('Contact is already exist');
+      this.reset();
       return;
     }
 
@@ -46,15 +47,15 @@ class ContactForm extends Component {
     this.reset();
   };
 
-  toggleAlert = (message) => {
+  toggleAlert = message => {
     this.setState({ showAlert: true, errorMessage: message });
     setTimeout(() => this.setState({ showAlert: false }), 1000);
   };
 
   reset = () => {
     this.setState({
-      name: "",
-      number: "",
+      name: '',
+      number: '',
     });
   };
 
@@ -99,11 +100,11 @@ class ContactForm extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   contacts: getContacts(state),
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   onSubmit: (newName, number) =>
     dispatch(contactsOperations.addContact(newName, number)),
 });
